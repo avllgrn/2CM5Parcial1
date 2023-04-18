8891d36543082c71a7e38f9351efe58209e5b5ff
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-04-18 07:55:56 -0600

    Distancia entre 2 puntos

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <math.h>
 #include "Punto.h"
 using namespace std;
 
 int main(void){
-    Punto P, Q, R;
+    Punto P1, P2;
+    double d;
 
     //1.Pide dato(s)
-    cout<<"Ingresa las coordenadas de P"<<endl;
-    P.pideleAlUsuarioTusAtributos();
-    cout<<"Ingresa las coordenadas de Q"<<endl;
-    Q.pideleAlUsuarioTusAtributos();
+    cout<<"Ingresa las coordenadas de P1"<<endl;
+    P1.pideleAlUsuarioTusAtributos();
+    cout<<"Ingresa las coordenadas de P2"<<endl;
+    P2.pideleAlUsuarioTusAtributos();
 
     //2.Calcula formula(s)
-    R.modificaTuAtributoX(P.retornaTuAtributoX()+Q.retornaTuAtributoX());
-    R.modificaTuAtributoY(P.retornaTuAtributoY()+Q.retornaTuAtributoY());
+    d = sqrt(pow(P2.retornaTuAtributoY()-P1.retornaTuAtributoY(),2)
+             +
+             pow(P2.retornaTuAtributoX()-P1.retornaTuAtributoX(),2)
+             );
 
     //3.Muestra resultado(s)
-    cout<<"P";P.muestraTusAtributos();cout<<endl;
-    cout<<"Q";Q.muestraTusAtributos();cout<<endl;
-    cout<<"R";R.muestraTusAtributos();cout<<endl;
+    cout<<"P1";P1.muestraTusAtributos();cout<<endl;
+    cout<<"P2";P2.muestraTusAtributos();cout<<endl;
+    cout<<"d = "<<d<<endl;
 
     return 0;
 }
