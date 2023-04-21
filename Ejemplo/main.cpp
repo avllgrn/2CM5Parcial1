0b172f78f6544ce6e989d118ed875b77ed1d21d0
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-04-21 09:39:47 -0500

    Suma de Complejos con funcion con retorno con argumentos

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -2,16 +2,9 @@
 #include "Complejo.h"
 using namespace std;
 
-void sumaComplejos(void);
+Complejo sumaComplejos(Complejo C1, Complejo C2);
 
 int main(void){
-
-    sumaComplejos();
-
-    return 0;
-}
-
-void sumaComplejos(void){
     Complejo C1, C2, C3;
 
     //1.Pide dato(s)
@@ -21,17 +14,29 @@ void sumaComplejos(void){
     C2.pideleAlUsuarioTusAtributos();
 
     //2.Calcula formula(s)
-    C3.modificaTuAtributoReal(C1.retornaTuAtributoReal()
-                              +
-                              C2.retornaTuAtributoReal()
-       );
-    C3.modificaTuAtributoImaginario(C1.retornaTuAtributoImaginario()
-                                    +
-                                    C2.retornaTuAtributoImaginario()
-       );
+    C3 = sumaComplejos(C1,C2);
 
     //3.Muestra resultado(s)
     cout<<"C1 = ";C1.muestraTusAtributos();cout<<endl;
     cout<<"C2 = ";C2.muestraTusAtributos();cout<<endl;
     cout<<"C3 = ";C3.muestraTusAtributos();cout<<endl;
+    return 0;
+}
+
+Complejo sumaComplejos(Complejo C1, Complejo C2){
+    Complejo C3;
+
+    //2.Calcula formula(s)
+    C3.modificaTuAtributoReal(
+          C1.retornaTuAtributoReal()
+          +
+          C2.retornaTuAtributoReal()
+       );
+    C3.modificaTuAtributoImaginario(
+        C1.retornaTuAtributoImaginario()
+        +
+        C2.retornaTuAtributoImaginario()
+       );
+
+    return C3;
 }
